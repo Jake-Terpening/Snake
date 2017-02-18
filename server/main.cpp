355022a44889692b9c2dd750d0d0f4a7218cf74d
Whitney Tran <whitnet@uci.edu>
2017-02-17 17:59:47 -0800

    Added stuff needed to split messages and update gamestate in server

--- server/main.cpp
+++ server/main.cpp
@@ -11,24 +11,26 @@
 #include <sstream>
 #include <time.h>
 #include "websocket.h"
+#include "gamestate.h"
 
 using namespace std;
 
 webSocket server;
-map<int, int> playerScores;
+gamestate State;
+map<int, string> players; //keeps track of which client is which snake
 
 /* called when a client connects */
+/* tracks clients to players*/
 void openHandler(int clientID) {
-	vector<int> clientIDs = server.getClientIDs();
 
-	// only one client can connect
-	if (clientIDs.size() == 1) {
-		std::cout << clientID << " joined." << std::endl;
-		playerScores[1] = 0;
-		playerScores[2] = 0;
+	std::cout << clientID << " joined." << std::endl;
+
+	if (State.empty()) {
+		State[clientID] = "p1";
 	}
-
-	// this disconnects anyone else
+	else if (State.size() == 1) {
+		State[clientID] = "p2";
+	}
 	else {
 		server.wsClose(clientID);
 	}
@@ -37,44 +39,42 @@ void openHandler(int clientID) {
 /* called when a client disconnects */
 void closeHandler(int clientID) {
 	std::cout << clientID << " disconnected." << std::endl;
+	State.erase(clientID);
 
-	vector<int> clientIDs = server.getClientIDs();
+}
+
+/* Return a vector of strings split over the delimiter for message*/
+vector<string> split(string message, char delimiter) {
+	vector<string> result = vector<string>();
+
+	stringstream stream(message);
+	string token;
+
+	while (getline(stream, token, delimiter)) {
+		result.push_back(token);
+	}
+
+	return result;
 }
 
 /* called when a client sends a message to the server */
 void messageHandler(int clientID, string message) {
-	ostringstream os;
+	vector<string> messageReceived = split(message, ); //need to fill
+}
 
-	// receive a string indicating who scored, mark on map, sends back updated score
-	if (message == "1") {
-		++playerScores[1];
-		std::cout << clientID << " scored." << std::endl;
-		os << "SCORE:" << playerScores[1];
-	}
-	else {
-		++playerScores[2];
-		std::cout << clientID << " scored." << std::endl;
-		os << "SCORE:" << playerScores[2];
-	}
+void moveHandler(int clientID, string direction) {
+	//given direction of client, apply game logic
+}
 
+/* this could probably just merge with messageHandler */
+void moveResults(int clientID, string message) {
+	//if needs SETUP and has 2 players, sends clients the ok for new game
+	//else should call on moveHandler to update game state and return update to client
 }
 
 /* called once per select() loop */
 void periodicHandler() {
-	static time_t next = time(NULL) + 10;
-	time_t current = time(NULL);
-	if (current >= next) {
-		ostringstream os;
-		string timestring = ctime(&current);
-		timestring = timestring.substr(0, timestring.size() - 1);
-		os << timestring;
-
-		vector<int> clientIDs = server.getClientIDs();
-		for (int i = 0; i < clientIDs.size(); i++)
-			server.wsSend(clientIDs[i], os.str());
-
-		next = time(NULL) + 10;
-	}
+	moveResults;
 }
 
 int main(int argc, char *argv[]) {
@@ -87,7 +87,7 @@ int main(int argc, char *argv[]) {
 	server.setOpenHandler(openHandler);
 	server.setCloseHandler(closeHandler);
 	server.setMessageHandler(messageHandler);
-	//server.setPeriodicHandler(periodicHandler);
+	server.setPeriodicHandler(periodicHandler); // to periodically check state
 
 	/* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
 	server.startServer(port);
