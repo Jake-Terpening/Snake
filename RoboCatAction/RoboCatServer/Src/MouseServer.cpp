e46ede6fdc9689f18e165b45390540fc72ceb78e
Daniel Williams <dtwilli1@uci.edu>
2017-03-06 23:13:07 -0800

    Alternate Snake Solution
    possible backup snake solution, if we want to use it

---
+++ RoboCatAction/RoboCatServer/Src/MouseServer.cpp
@@ -0,0 +1,23 @@
+#include <RoboCatServerPCH.h>
+
+MouseServer::MouseServer()
+{
+}
+
+void MouseServer::HandleDying()
+{
+	NetworkManagerServer::sInstance->UnregisterGameObject( this );
+}
+
+
+bool MouseServer::HandleCollisionWithCat( RoboCat* inCat )
+{
+	//kill yourself!
+	SetDoesWantToDie( true );
+
+	ScoreBoardManager::sInstance->IncScore( inCat->GetPlayerId(), 1 );
+	Server::sInstance->Run();
+	return false;
+}
+
+
